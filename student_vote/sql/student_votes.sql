1480d579dd5dab519e623296cdfcfcdd2d240346
John Perry <PerryJ1@student.tafe>
2016-11-07 15:44:45 +1000

    add venue voting system

---
+++ student_vote/sql/student_votes.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 07, 2016 at 06:05 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_votes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` double NOT NULL,
+  `fname` varchar(128) NOT NULL,
+  `lname` varchar(128) NOT NULL,
+  `salutation` varchar(24) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `fname`, `lname`, `salutation`) VALUES
+(5105490515, '', '', ''),
+(9103077704, '', '', ''),
+(1105393115, '', '', ''),
+(2100110115, '', '', ''),
+(3103779415, '', '', ''),
+(8100160612, '', '', ''),
+(4100182116, '', '', ''),
+(2101920515, '', '', ''),
+(6190023102, '', '', ''),
+(6100213116, '', '', ''),
+(8104334014, '', '', ''),
+(3105793414, '', '', ''),
+(4100681916, '', '', ''),
+(5102640913, '', '', ''),
+(7103292214, '', '', ''),
+(2103279814, '', '', ''),
+(8104930815, '', '', ''),
+(3105274215, '', '', ''),
+(3105645008, '', '', ''),
+(8100558216, '', '', ''),
+(3100220316, '', '', ''),
+(1100125716, '', '', ''),
+(6107683511, '', '', ''),
+(3100307116, '', '', ''),
+(7060190504, '', '', ''),
+(6103823213, '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+CREATE TABLE `votes` (
+  `vote_id` int(11) NOT NULL,
+  `student_no` double NOT NULL,
+  `vote_choice` varchar(128) NOT NULL,
+  `vote_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `votes`
+--
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `votes`
+--
+ALTER TABLE `votes`
+  ADD PRIMARY KEY (`vote_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `votes`
+--
+ALTER TABLE `votes`
+  MODIFY `vote_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ student_vote/vote.php
@@ -0,0 +1,107 @@
+<?PHP
+
+	function dbConnect() {
+		// SETUP CONNECTION TO DATABASE
+		$db_user = 'root'; $db_pass = ''; $db_uri = "mysql:dbname=student_votes;host=127.0.0.1";
+		$conn = new PDO($db_uri, $db_user, $db_pass);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		return $conn;
+	}
+
+	function getVenues() {
+		$connect = dbConnect();
+		$sql = "SELECT * FROM venue";
+		$conn = $connect->prepare($sql);
+		$conn->execute();
+		return $conn->fetchAll(PDO::FETCH_ASSOC);		
+	}
+
+	function doGetStudents() {
+		$connect = dbConnect();
+		$sql = "SELECT * FROM students";
+		$conn = $connect->prepare($sql);
+		$conn->execute();
+		return $conn->fetchAll(PDO::FETCH_ASSOC);		
+	}
+
+	function addVote($student, $vote) {
+		$connect = dbConnect();
+		$check_sql = "SELECT student_no FROM votes WHERE student_no = " . $student;
+		$conn = $connect->prepare($check_sql);
+		$conn->execute();
+		$result = $conn->fetchAll(PDO::FETCH_ASSOC);	
+
+		if(count($result) > 0) {
+			echo 'vote already made';
+		} else {
+			$check2_sql = "SELECT * FROM venue";
+			$conn = $connect->prepare($check2_sql);
+			$conn->execute();
+			$venues = $conn->fetchAll(PDO::FETCH_ASSOC);
+
+			if(array_search($vote, array_column($venues, 'venue_url'))) {
+				$sql = "INSERT INTO votes (student_no, vote_choice) VALUES ('" . $student . "', '" . $vote . "')";
+				$conn = $connect->prepare($sql);
+				$conn->execute();
+			} else {
+				echo 'invalid venue';
+			}
+		}
+	}
+
+	function doGetVotes() {
+		$connect = dbConnect();
+		$sql = "SELECT count(*) AS vote_count, vote_choice FROM votes GROUP BY vote_choice";
+		$conn = $connect->prepare($sql);
+		$conn->execute();
+		return $conn->fetchAll(PDO::FETCH_ASSOC);		
+    }
+
+	if(isset($_POST['student_no'])) {
+		$students = doGetStudents();
+		if(array_search($_POST['student_no'] ,array_column($students,'id'))) {
+			addVote($_POST['student_no'], $_POST['voted_place']);
+		} else {
+			echo 'invalid Student Number';
+		}
+	}
+    
+    ?>
+
+<html>
+	<head>
+		<link rel="stylesheet" href="css/style.css">
+		<!-- <meta http-equiv="refresh" content="30"> -->
+		<script>
+		</script>
+	</head>
+	<body>	
+	<div class="header_img">
+    	<img src="img/ROLM_logo_txt.png">
+	</div>
+
+		<form method="POST" action="./vote.php">
+			<input class="formlight" name="student_no" type="number" size="24" placeholder="Student Number"> 
+			<select class="formlight" name="voted_place">
+			<?php 
+			$venues = getVenues();
+			foreach($venues as $venue) {
+				echo '<option value="' . $venue['venue_url'] . '">' . $venue['venue_url'] . '</option>';
+			}
+			?>
+			</select>
+			<input class="formborderlight" type="submit" name="submit" value="vote">
+		</form>
+
+
+<?PHP
+// Display all SSIDs and wether they have been found or not
+	$votes = doGetVotes();	
+
+	foreach($votes as $vote) {
+	    echo '<section id="' . $vote['vote_choice'] . '" class="highlight"><span>' .  $vote['vote_choice'] . '</span><span>' . $vote['vote_count'] . '</span></section>';
+	}
+?>
+
+	</body>
+</html>
