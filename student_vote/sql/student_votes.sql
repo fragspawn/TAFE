817cf06c91453216659d3adde801b0412a267ed4
John Perry <PerryJ1@student.tafe>
2016-11-07 16:12:30 +1000

    security fix, add location to sql

--- student_vote/sql/student_votes.sql
+++ student_vote/sql/student_votes.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2016 at 06:05 AM
+-- Generation Time: Nov 07, 2016 at 07:00 AM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -14,7 +14,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `student_votes`
@@ -68,6 +67,26 @@ INSERT INTO `students` (`id`, `fname`, `lname`, `salutation`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `venue`
+--
+
+CREATE TABLE `venue` (
+  `venue_url` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `venue`
+--
+
+INSERT INTO `venue` (`venue_url`) VALUES
+('pignwhistle.com.au'),
+('thefox.com.au'),
+('theshipinn.com.au'),
+('ploughinn.com.au');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `votes`
 --
 
@@ -75,13 +94,8 @@ CREATE TABLE `votes` (
   `vote_id` int(11) NOT NULL,
   `student_no` double NOT NULL,
   `vote_choice` varchar(128) NOT NULL,
-  `vote_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `vote_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `votes`
---
-
 
 --
 -- Indexes for dumped tables
@@ -101,7 +115,7 @@ ALTER TABLE `votes`
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `vote_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `vote_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
